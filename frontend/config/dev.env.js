1e4f457c7a543daa482770975b60168ddd4680cc
eliteluxury <elicloudh@gmail.com>
2019-03-12 11:03:28 -0400

    fix env issue in frontend

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"http://localhost:3000/api"'
+  NODE_ENV: '"development"'
 })
