3532684cd1f3e749d9b49422425c22019c47c984
eliteluxury <elicloudh@gmail.com>
2019-03-29 16:10:30 -0400

    updated template of TaxCompareV2

--- frontend/src/pages/TaxCompareV2.vue
+++ frontend/src/pages/TaxCompareV2.vue
@@ -10,7 +10,7 @@
          <div class="form-group">
             <input type="text" name="contract" id="contract" value="10000000" class="selectpicker" v-model="state.amountFieldValue">
          </div>
-         <label for="years">Years</label>
+         <!-- <label for="years">Years</label>
          <div class="form-group">
 
             <select name="years" id="years" class="selectpicker" value="5" v-model="state.lengthFieldValue">
@@ -27,23 +27,23 @@
                <option value="11">11</option>
                <option value="12">12</option>
                </select>
-         </div>
+         </div> -->
 
          <!-- ELMER POPULATE TEAMS HERE BASED OFF OF TEAMS IN THE INDEX -->
-         <label for="team1">Offer From</label>
+         <!-- <label for="team1">Offer From</label>
          <div class="form-group">
             <select name="team1" id="team1" class="selectpicker" v-model="state.team1FieldValue">
                <option v-for="(taxIndex, key) in taxIndexes" :key="key" :value="taxIndex.Id">{{ taxIndex.Team }}</option>
             </select>
-         </div>
+         </div> -->
          <!-- ELMER POPULATE TEAMS HERE BASED OFF OF TEAMS IN THE INDEX -->
-         <label for="team2">Competing Offer(s) For</label>
+         <!-- <label for="team2">Competing Offer(s) For</label>
          <div class="form-group">
             <select name="team2" id="team2" class="selectpicker" v-model="state.team2FieldValue">
                <option v-for="(taxIndex, key) in taxIndexes" :key="key" :value="taxIndex.Id">{{ taxIndex.Team }}</option>
             </select>
-         </div>
-         <label for="escrow">Escrow</label>
+         </div> -->
+         <!-- <label for="escrow">Escrow</label>
          <div class="form-group">
             <select name="escrow" id="escrow" class="selectpicker" v-model="state.escrowFieldValue">
                <option value="0.00">0%</option>
@@ -68,10 +68,10 @@
                <option value="0.19">19%</option>
                <option value="0.20">20%</option>
             </select>
-         </div>
+         </div> -->
 
          <!-- DON"T NEED TO EDIT -->
-         <h4>Present Value</h4>
+         <!-- <h4>Present Value</h4>
          <label for="discountrate">Discount Rate</label>
          <div class="form-group">
             <select name="discountrate" id="discountrate" class="selectpicker" v-model="state.discountRateFieldValue">
@@ -89,7 +89,7 @@
                <option value="0.11">0.11</option>
                <option value="0.12">0.12</option>
                </select>
-         </div>
+         </div> -->
          <div class="clearfix mtop15"></div>
          <button class="button btn btn-primary pull-right mtop15" id="submit-button" @click="handleCompareSubmit()">Submit</button>
       </div>
@@ -97,7 +97,7 @@
 
    <div class="col-lg-18 col-md-18 col-sm-24">
       <h2>Earnings</h2>
-      {{state.team1}}
+      <!-- {{state.team1}} -->
       <div class="row">
          <div class="col-lg-24 col-md-24 card">
             <div class="row">
@@ -143,45 +143,25 @@ export default {
   ],
   mounted () {
     this.fetch()
-    this.setup()
   },
 
   data () {
     return {
-      defaultState: {
-        amountFieldValue: null,
-        lengthFieldValue: null,
-        escrowFieldValue: null,
-        discountRateFieldValue: null,
-        team1FieldValue: null,
-        team2FieldValue: null
-      },
       state: {
         isLoading: false,
         isDirty: false,
-        amountFieldValue: null,
-        lengthFieldValue: null,
-        escrowFieldValue: null,
-        discountRateFieldValue: null,
-        team1FieldValue: null,
-        team2FieldValue: null,
-        team1: null
+        amountFieldValue: 10000000
+        // lengthFieldValue: null,
+        // escrowFieldValue: null,
+        // discountRateFieldValue: null,
+        // team1FieldValue: null,
+        // team2FieldValue: null,
+        // team1: null
       },
-      taxIndexes: {},
-      comparedTaxIndexes: {}
+      taxIndexes: {}
     }
   },
   methods: {
-    setup () {
-      this.defaultState.amountFieldValue = 10000000
-      this.defaultState.lengthFieldValue = 5
-      this.defaultState.escrowFieldValue = '0.00'
-      this.defaultState.discountRateFieldValue = '0.07'
-      this.defaultState.team1FieldValue = '1'
-      this.defaultState.team2FieldValue = '2'
-
-      Object.assign(this.state, this.defaultState)
-    },
     fetch () {
       api.getTaxIndexes(this.$route.params.league_id)
         .then(res => {
@@ -190,29 +170,21 @@ export default {
         })
     },
     handleCompareSubmit () {
-      var params = {
-        contractAmount: this.state.amountFieldValue,
-        contractLength: this.state.lengthFieldValue,
-        escrow: this.state.escrowFieldValue,
-        discountRate: this.state.discountRateFieldValue,
-        team1: this.state.team1FieldValue,
-        team2: this.state.team2FieldValue
-      }
+      this.fetch()
+      // var params = {
+      //   contractAmount: this.state.amountFieldValue,
+      //   contractLength: this.state.lengthFieldValue,
+      //   escrow: this.state.escrowFieldValue,
+      //   discountRate: this.state.discountRateFieldValue,
+      //   team1: this.state.team1FieldValue,
+      //   team2: this.state.team2FieldValue
+      // }
 
-      api.getTaxIndexCompare(this.$route.params.league_id, params)
-        .then(res => {
-          console.log(res.data)
-          this.state.team1 = res.data.comparision.team1
-        })
-    },
-    matchedContract (team2) {
-      let t1 = this.state.team1.tax
-      let t2 = team2
-      if (t1.netIncome > t2.netIncome) {
-        return t1.netIncome / (1 - t2.taxRate)
-      } else {
-        return t1.netIncome / (1 - t2.taxRate)
-      }
+    //   api.getTaxIndexCompare(this.$route.params.league_id, params)
+    //     .then(res => {
+    //       console.log(res.data)
+    //       this.state.team1 = res.data.comparision.team1
+    //     })
     }
   }
 }
