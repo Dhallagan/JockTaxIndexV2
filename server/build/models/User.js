00a6ad51ab9c70537dddbc1381cc3679b8d4253b
eliteluxury <elicloudh@gmail.com>
2019-05-23 23:39:33 -0400

    delete build directory of server

--- server/build/models/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Property = /** @class */ (function () {
-    function Property() {
-    }
-    return Property;
-}());
-exports.Property = Property;
--- server/build/repositories/welcome.repository.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WelcomeRepository = /** @class */ (function () {
-    function WelcomeRepository() {
-    }
-    return WelcomeRepository;
-}());
-exports.WelcomeRepository = WelcomeRepository;
